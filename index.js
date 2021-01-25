8378422f6c5c6992d8eae0506afcb46ac49efcfa
SamuelX <samxiezx1115@gmail.com>
2021-01-25 11:21:55 -0500

    font size fixed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const menu_frame = document.getElementById('menu_frame');
 const close_button = document.getElementById('close_button');
 const login_button = document.getElementById('login_arc');
 const more_dropdown = document.getElementById('more_options');
+const sidebar_option = document.getElementById('sidebar_option')
+
 const extra_options = document.getElementById('extra_options')
 const more_options = document.getElementById('more_options');
 
@@ -41,8 +43,13 @@ login_button.addEventListener('click',()=>{
 //dropDown feature in the menu bar
 more_dropdown.addEventListener('click',()=>{
     console.log('Dropdown button clicked.');
+
+    const height = sidebar_option.clientHeight;
+    const str_height = height*3+'px'
+    console.log(str_height);
+
     if(extra_options.style.height==='0px'){
-        extra_options.style.height='150px';
+        extra_options.style.height=str_height;
         extra_options.style.color='black';
         more_options.textContent='Less';
     }else{
