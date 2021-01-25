42e4113829bb6162d0c4589e25e9c1f53a37b884
SamuelX <samxiezx1115@gmail.com>
2021-01-25 17:53:12 -0500

    final clicks

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ const login_button = document.getElementById('login_arc');
 var sidebar_option = document.getElementById('sidebar_option');
 var extra_options = document.getElementById('extra_options')
 var more_options = document.getElementById('more_options');
+
 var height = sidebar_option.clientHeight;
+var counter = 0 //unclicked
+
+
+menu_button.addEventListener('click',()=>{
+    if(counter===1){
+        menu_button.style.transform="rotate(90deg)";
+        menu_button.style.marginLeft="30.5%";
+        menu_frame.style.marginLeft="0";
+    }else{
+        alert('Please click on the Login button first.')
+    }
+})
 
 //top left, close menu button
 close_button.addEventListener('click',()=>{
     let timer = 0;
+    login_button.textContent='Login'
+    counter=0
 
     if(extra_options.style.height!=='0px'){
         extra_options.style.height ='0px'
@@ -36,9 +51,19 @@ close_button.addEventListener('click',()=>{
 
 //top right, corner login button click
 login_button.addEventListener('click',()=>{
-    menu_button.style.transform="rotate(90deg)";
-    menu_button.style.marginLeft="30.5%";
-    menu_frame.style.marginLeft="0";
+    if(counter===0){
+        counter=1 //logged in
+        login_button.textContent='Loggedin'
+    }else{
+        
+        login_button.textContent='Login'
+        counter=0 //logged out
+    }
+    console.log(counter)
+
+    // menu_button.style.transform="rotate(90deg)";
+    // menu_button.style.marginLeft="30.5%";
+    // menu_frame.style.marginLeft="0";
     const entire_doc_height = document.body.scrollHeight
     window.scrollTo(0,entire_doc_height);
 })
